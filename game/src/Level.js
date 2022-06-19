4868b212b1522f7b27741489361b503c0897ecc6
diogosmac <diogo.smac@gmail.com>
2022-06-19 18:37:14 +0100

    extracted level setup functions to separate module

---
+++ game/src/Level.js
@@ -0,0 +1,198 @@
+import { CST } from './CST'
+
+export const LEVEL = {
+    /**
+     * +++ AUX FUNCTIONS
+     */
+    setupInterface(level) {
+        /**
+         * @@@  Centering text:
+         *      x = position_x + (width / 2)
+         *      y = position_y + (height / 2)
+         *      Then, place the text in (x,y), and call setOrigin(0.5)
+         */
+
+        // // set background
+        level.add.image(0, 0, CST.LEVEL.BACKGROUND).setOrigin(0).setDepth(0)
+
+        // // set weather on top left
+        level.add.image(53, 24, level.level_data.weather).setOrigin(0).setDepth(1)
+        // // // set temperature and icon
+        level.add.text(153.5, 41.5, level.level_data.temperature + 'ºC', CST.STYLES.FLAVOR_SMALL).setOrigin(0.5)
+        level.add.image(177, 29, CST.ICONS.TEMPERATURE).setOrigin(0).setDepth(1)
+        // // // set humidity and icon
+        level.add.text(153.5, 76.5, level.level_data.humidity + '%', CST.STYLES.FLAVOR_SMALL).setOrigin(0.5)
+        level.add.image(177, 64, CST.ICONS.HUMIDITY).setOrigin(0).setDepth(1)
+
+        // // set max battery for level on top right
+        level.add.image(695.55, 28, CST.ICONS.BATTERY).setOrigin(0).setDepth(1)
+        level.add.text(661.78, 64, level.level_data.max_battery + '%', CST.STYLES.FLAVOR_LARGE).setOrigin(0.5)
+
+        // // place map and power-up bar placeholders
+        level.add.image(53, 136, CST.PLACEHOLDER.MAP).setOrigin(0).setDepth(1)
+        level.add.image(688, 136, CST.PLACEHOLDER.POWERUPS).setOrigin(0).setDepth(1)
+
+        // // add destination selection outline image
+        level.visual.dest_outline = level.add.image(
+            0,
+            18,
+            CST.DEST.SELECTED
+        ).setOrigin(0).setDepth(1).setVisible(false)
+    },
+
+    // place possible destinations on top of screen
+    placeDestinationSquare(level, i) {
+        let dest = level.gameplay.destinations[i]
+        dest.icon = level.add.image(
+            dest.screen_position.x,
+            dest.screen_position.y,
+            dest.icon
+        ).setOrigin(0).setDepth(1)
+        dest.icon_out = level.add.image(
+            dest.screen_position.x,
+            dest.screen_position.y,
+            dest.icon_out
+        ).setOrigin(0).setDepth(1).setVisible(false)
+        dest.icon.setInteractive()
+        dest.icon.on(CST.MOUSE.CLICK_RELEASE, () => {
+            if (i === level.gameplay.selected_route) {
+                level.gameplay.selected_route = null
+                level.visual.dest_outline.setVisible(false)
+            } else if (dest.uses > 0) {
+                level.gameplay.selected_route = i
+                level.visual.dest_outline.x = dest.screen_position.x
+                level.visual.dest_outline.setVisible(true)
+            }
+        })
+        dest.text = level.add.text(
+            dest.screen_position.x + 68.63,
+            dest.screen_position.y + 13.56,
+            dest.uses,
+            CST.STYLES.DEST_USES
+        ).setOrigin(0.5).setDepth(2)
+    },
+
+    updateDestDisplay(dest) {
+        dest.icon.setVisible(dest.uses > 0)
+        dest.icon_out.setVisible(dest.uses === 0)
+        dest.text.setText(dest.uses)
+    },
+
+    // place route slots on bottom of screen
+    placeRouteSlot(level, i, x, y) {
+        if (i) {
+            level.visual.openRoutes.push(
+                level.add.image(x, y, CST.LEVEL.ROUTE.OPEN).setOrigin(0).setDepth(1)
+            )
+            if (i > 1) {
+                level.visual.closedRoutes.push(
+                    level.add.image(x, y, CST.LEVEL.ROUTE.CLOSED).setOrigin(0).setDepth(2)
+                )
+            }
+        } else {
+            let icon = level.add.image(x, y, level.gameplay.routes[0].icon).setOrigin(0).setDepth(1)
+            level.gameplay.routes[0].icon = icon
+        }
+    },
+
+    // place battery bar and cut usable portion based on percentage
+    setupBatteryBar(level) {
+        /**
+         * @@@ INFO ON CROPPING IMAGES
+         * https://www.html5gamedevs.com/topic/36973-showing-cropped-image/
+         */
+        let full = level.add.image(53, 528, CST.LEVEL.BATTERY.FULL).setOrigin(0).setDepth(1)
+        let usable = level.add.image(53, 528, CST.LEVEL.BATTERY.USABLE).setOrigin(0).setDepth(2)
+        usable.frame.cutWidth = full.frame.width * level.level_data.max_battery_dec
+        usable.frame.updateUVs();
+        level.add.text(542.64, 546.5, level.gameplay.current_battery, CST.STYLES.BATTERY_PERCENTAGE).setOrigin(0.5).setDepth(3)
+    },
+
+    checkAndRemoveRoute(level, curr) {
+        let next = curr + 1
+        if (
+            level.gameplay.routes[curr] !== undefined &&
+            level.gameplay.routes[next] === undefined
+        ) {
+            let route = level.gameplay.routes[curr]
+            let dest = level.gameplay.destinations[route.name]
+            route.icon.destroy()
+            dest.uses++
+            LEVEL.updateDestDisplay(dest)
+            level.gameplay.routes.pop(curr)
+            level.visual.closedRoutes[level.gameplay.next_route].setVisible(true)
+            level.gameplay.next_route--
+        }
+    },
+
+    setupRoutes(level) {
+        // iterating over indexes, not the actual closed routes
+        for (let i in level.visual.closedRoutes) {
+            if (i < 1) { continue }
+            level.visual.openRoutes[i].setInteractive()
+            if (i > 1) level.visual.closedRoutes[i].setInteractive()
+            level.visual.openRoutes[i].on(CST.MOUSE.CLICK, (pointer) => {
+                let curr = parseInt(i)
+                if (pointer.rightButtonDown()) {
+                    let next = curr + 1
+                    if (
+                        level.gameplay.routes[curr] !== undefined &&
+                        level.gameplay.routes[next] === undefined
+                    ) {
+                        let route = level.gameplay.routes[curr]
+                        let dest = level.gameplay.destinations[route.name]
+                        route.icon.destroy()
+                        dest.uses++
+                        LEVEL.updateDestDisplay(dest)
+                        level.gameplay.routes.pop(curr)
+                        level.visual.closedRoutes[level.gameplay.next_route].setVisible(true)
+                        level.gameplay.next_route--
+                    }
+                    return
+                }
+
+                if (level.gameplay.selected_route !== null) {
+                    if (level.gameplay.routes[curr] !== undefined) {
+                        return
+                    }
+                    let selected = level.gameplay.selected_route
+                    let last = level.gameplay.routes[level.gameplay.next_route - 1]
+                    if (selected !== last.name) {
+                        level.gameplay.routes[level.gameplay.next_route] = {
+                            name: selected,
+                            icon: level.add.image(
+                                level.visual.openRoutes[level.gameplay.next_route].x,
+                                level.visual.openRoutes[level.gameplay.next_route].y,
+                                level.gameplay.destinations[level.gameplay.selected_route].route
+                            ).setOrigin(0).setDepth(2),
+                            occupied: true
+                        }
+                        level.gameplay.next_route++
+                        level.visual.closedRoutes[level.gameplay.next_route].setVisible(false)
+                        let dest = level.gameplay.destinations[selected]
+                        dest.uses--
+                        LEVEL.updateDestDisplay(dest)
+                        level.gameplay.selected_route = null
+                        level.visual.dest_outline.setVisible(false)
+                    }
+                }
+            })
+        }
+    },
+
+    setupGoButton(level) {
+        let goUnusable = level.add.image(593, 528, CST.LEVEL.GO.UNUSABLE).setOrigin(0).setDepth(1)
+        let goButton = level.add.image(593, 528, CST.LEVEL.GO.USABLE).setOrigin(0).setDepth(1)
+
+        goButton.setVisible(false)
+
+        goUnusable.setInteractive()
+        goUnusable.on(CST.MOUSE.HOVER, () => {
+            goButton.setVisible(true)
+        })
+        goUnusable.on(CST.MOUSE.LEAVE, () => {
+            goButton.setVisible(false)
+        })
+    },
+
+}
--- game/src/scenes/LevelScene.js
+++ game/src/scenes/LevelScene.js
@@ -1,4 +1,5 @@
 import { CST } from '../CST'
+import { LEVEL } from '../Level'
 
 export class LevelScene extends Phaser.Scene {
     constructor() {
@@ -56,6 +57,13 @@ export class LevelScene extends Phaser.Scene {
             ],
         }
 
+    }
+    preload() {
+
+    }
+    create() {
+
+        // constant between levels - will be in general "generateLevel" function
         this.visual = {
             // open routes are the white squares that signal an available route slot
             // the first slot is always occupied (home)
@@ -64,204 +72,24 @@ export class LevelScene extends Phaser.Scene {
             // first and second routes will never be closed, as the first route is always set
             closedRoutes: [null, null],
         }
-    }
-    preload() {
 
-    }
-    create() {
         this.input.mouse.disableContextMenu()
-        this.setupInterface()
+        LEVEL.setupInterface(this)
 
         for (let i in this.gameplay.destinations) {
-            this.placeDestinationSquare(i)
+            LEVEL.placeDestinationSquare(this, i)
         }
 
-        this.setupBatteryBar()
+        LEVEL.setupBatteryBar(this)
 
         for (let i = 0; i < 8; i++) {
-            this.placeRouteSlot(i, 53 + i * 90, 429)
+            LEVEL.placeRouteSlot(this, i, 53 + i * 90, 429)
         }
 
-        // iterating over indexes, not the actual closed routes
-        for (let i in this.visual.closedRoutes) {
-            if (i < 1) { continue }
-            this.visual.openRoutes[i].setInteractive()
-            if (i > 1) this.visual.closedRoutes[i].setInteractive()
-            this.visual.openRoutes[i].on(CST.MOUSE.CLICK, (pointer) => {
-                let curr = parseInt(i)
-                if (pointer.rightButtonDown()) {
-                    let next = curr + 1
-                    if (
-                        this.gameplay.routes[curr] !== undefined &&
-                        this.gameplay.routes[next] === undefined
-                    ) {
-                        let route = this.gameplay.routes[curr]
-                        let dest = this.gameplay.destinations[route.name]
-                        route.icon.destroy()
-                        dest.uses++
-                        this.updateDestDisplay(dest)
-                        this.gameplay.routes.pop(curr)
-                        this.visual.closedRoutes[this.gameplay.next_route].setVisible(true)
-                        this.gameplay.next_route--
-                    }
-                    return
-                }
+        LEVEL.setupRoutes(this)
 
-                if (this.gameplay.selected_route !== null) {
-                    if (this.gameplay.routes[curr] !== undefined) {
-                        return
-                    }
-                    let selected = this.gameplay.selected_route
-                    let last = this.gameplay.routes[this.gameplay.next_route - 1]
-                    if (selected !== last.name) {
-                        this.gameplay.routes[this.gameplay.next_route] = {
-                            name: selected,
-                            icon: this.add.image(
-                                this.visual.openRoutes[this.gameplay.next_route].x,
-                                this.visual.openRoutes[this.gameplay.next_route].y,
-                                this.gameplay.destinations[this.gameplay.selected_route].route
-                            ).setOrigin(0).setDepth(2),
-                            occupied: true
-                        }
-                        this.gameplay.next_route++
-                        this.visual.closedRoutes[this.gameplay.next_route].setVisible(false)
-                        let dest = this.gameplay.destinations[selected]
-                        dest.uses--
-                        this.updateDestDisplay(dest)
-                        this.gameplay.selected_route = null
-                        this.visual.dest_outline.setVisible(false)
-                    }
-                }
-            })
-        }
-
-        let goUnusable = this.add.image(593, 528, CST.LEVEL.GO.UNUSABLE).setOrigin(0).setDepth(1)
-        let goButton = this.add.image(593, 528, CST.LEVEL.GO.USABLE).setOrigin(0).setDepth(1)
-
-        goButton.setVisible(false)
-
-        goUnusable.setInteractive()
-        goUnusable.on(CST.MOUSE.HOVER, () => {
-            goButton.setVisible(true)
-        })
-        goUnusable.on(CST.MOUSE.LEAVE, () => {
-            goButton.setVisible(false)
-        })
-        /**
-         * *** End of Progress Update Demo Code
-         */
+        LEVEL.setupGoButton(this)
 
     }
 
-    /**
-     * +++ AUX FUNCTIONS
-     */
-    setupInterface() {
-        /**
-         * @@@  Centering text:
-         *      x = position_x + (width / 2)
-         *      y = position_y + (height / 2)
-         *      Then, place the text in (x,y), and call setOrigin(0.5)
-         */
-
-        // // set background
-        this.add.image(0, 0, CST.LEVEL.BACKGROUND).setOrigin(0).setDepth(0)
-
-        // // set weather on top left
-        this.add.image(53, 24, this.level_data.weather).setOrigin(0).setDepth(1)
-        // // // set temperature and icon
-        this.add.text(153.5, 41.5, this.level_data.temperature + 'ºC', CST.STYLES.FLAVOR_SMALL).setOrigin(0.5)
-        this.add.image(177, 29, CST.ICONS.TEMPERATURE).setOrigin(0).setDepth(1)
-        // // // set humidity and icon
-        this.add.text(153.5, 76.5, this.level_data.humidity + '%', CST.STYLES.FLAVOR_SMALL).setOrigin(0.5)
-        this.add.image(177, 64, CST.ICONS.HUMIDITY).setOrigin(0).setDepth(1)
-
-        // // set max battery for level on top right
-        this.add.image(695.55, 28, CST.ICONS.BATTERY).setOrigin(0).setDepth(1)
-        this.add.text(661.78, 64, this.level_data.max_battery + '%', CST.STYLES.FLAVOR_LARGE).setOrigin(0.5)
-
-        // // place map and power-up bar placeholders
-        this.add.image(53, 136, CST.PLACEHOLDER.MAP).setOrigin(0).setDepth(1)
-        this.add.image(688, 136, CST.PLACEHOLDER.POWERUPS).setOrigin(0).setDepth(1)
-
-        // // add destination selection outline image
-        this.visual.dest_outline = this.add.image(
-            0,
-            18,
-            CST.DEST.SELECTED
-        ).setOrigin(0).setDepth(1).setVisible(false)
-    }
-
-    // place possible destinations on top of screen
-    placeDestinationSquare(i) {
-        let dest = this.gameplay.destinations[i]
-        dest.icon = this.add.image(
-            dest.screen_position.x,
-            dest.screen_position.y,
-            dest.icon
-        ).setOrigin(0).setDepth(1)
-        dest.icon_out = this.add.image(
-            dest.screen_position.x,
-            dest.screen_position.y,
-            dest.icon_out
-        ).setOrigin(0).setDepth(1).setVisible(false)
-        // let route = this.add.image(
-        //     dest.screen_position.x,
-        //     dest.screen_position.y,
-        //     dest.icon
-        // ).setOrigin(0).setDepth(1)
-        dest.icon.setInteractive()
-        dest.icon.on(CST.MOUSE.CLICK_RELEASE, () => {
-            if (i === this.gameplay.selected_route) {
-                this.gameplay.selected_route = null
-                this.visual.dest_outline.setVisible(false)
-            } else if (dest.uses > 0) {
-                this.gameplay.selected_route = i
-                this.visual.dest_outline.x = dest.screen_position.x
-                this.visual.dest_outline.setVisible(true)
-            }
-        })
-        dest.text = this.add.text(
-            dest.screen_position.x + 68.63,
-            dest.screen_position.y + 13.56,
-            dest.uses,
-            CST.STYLES.DEST_USES
-        ).setOrigin(0.5).setDepth(2)
-    }
-
-    updateDestDisplay(dest) {
-        dest.icon.setVisible(dest.uses > 0)
-        dest.icon_out.setVisible(dest.uses === 0)
-        dest.text.setText(dest.uses)
-    }
-
-    // place route slots on bottom of screen
-    placeRouteSlot(i, x, y) {
-        if (i) {
-            this.visual.openRoutes.push(
-                this.add.image(x, y, CST.LEVEL.ROUTE.OPEN).setOrigin(0).setDepth(1)
-            )
-            if (i > 1) {
-                this.visual.closedRoutes.push(
-                    this.add.image(x, y, CST.LEVEL.ROUTE.CLOSED).setOrigin(0).setDepth(2)
-                )
-            }
-        } else {
-            let icon = this.add.image(x, y, this.gameplay.routes[0].icon).setOrigin(0).setDepth(1)
-            this.gameplay.routes[0].icon = icon
-        }
-    }
-
-    // place battery bar and cut usable portion based on percentage
-    setupBatteryBar() {
-        /**
-         * @@@ INFO ON CROPPING IMAGES
-         * https://www.html5gamedevs.com/topic/36973-showing-cropped-image/
-         */
-        let full = this.add.image(53, 528, CST.LEVEL.BATTERY.FULL).setOrigin(0).setDepth(1)
-        let usable = this.add.image(53, 528, CST.LEVEL.BATTERY.USABLE).setOrigin(0).setDepth(2)
-        usable.frame.cutWidth = full.frame.width * this.level_data.max_battery_dec
-        usable.frame.updateUVs();
-        this.add.text(542.64, 546.5, this.gameplay.current_battery, CST.STYLES.BATTERY_PERCENTAGE).setOrigin(0.5).setDepth(3)
-    }
 }
